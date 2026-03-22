109cc8a65da86ed5260fd6e36f3aa726379b6425
USER\trcc8 <ring3367@naver.com>
2026-03-22 17:42:14 +0900

    [260322] test

--- src/sec01/exam01/VariableInitializationExample.java
+++ src/sec01/exam01/VariableInitializationExample.java
@@ -4,7 +4,7 @@ public class VariableInitializationExample {
     public static void main(String[] args) {
         int value;
 
- //     int result = value + 10;
+        //     int result = value + 10;
 
 //      System.out.println(result);
     }
